2ec2379790979de6957e605f9b96b5abd3b40629
Hervé Boutemy <hboutemy@apache.org>
2020-09-04 13:36:47 +0200

    add webpack-copy configuration

---
+++ custom-webpack.config.js
@@ -0,0 +1,10 @@
+const CopyModulesPlugin = require('copy-modules-webpack-plugin');
+
+module.exports = {
+  plugins: [
+    new CopyModulesPlugin({
+      destination: 'webpack-modules',
+      includePackageJsons: true,
+    }),
+  ],
+};
